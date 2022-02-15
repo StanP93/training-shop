f16fdece56ccc755a4190d54150a2893eb473953
Stanislau Pranovich <stanislaupranovich@MacBook-Air-Stanislau.local>
2022-02-15 19:01:36 +0300

    fixed data-test-id

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div className="App" data-test-id ='app'>
       <Header/>
       <Title/>
-      <Clothing sex={'women'} data-test-id={`clothes-${women}`}/>
-      <Clothing sex={'men'} data-test-id={`clothes-${men}`}/>
+      <Clothing sex={'women'} data-test-id={`clothes-women`}/>
+      <Clothing sex={'men'} data-test-id={`clothes-men`}/>
       <LookBook />
       <Subscribe />
       <Blog />
